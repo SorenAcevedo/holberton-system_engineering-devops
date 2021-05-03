64410cfe784f2cd907a75c89be6b345dfb6c0293
SorenAcevedo <sorenacevedo@gmail.com>
2021-05-03 18:50:15 -0500

    Change dict content

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Module that use REST API """
-import requests
 import json
+import requests
 
 
 if __name__ == "__main__":
@@ -11,12 +11,17 @@ if __name__ == "__main__":
     response_todo = requests.get(url_todo)
     response_user = requests.get(url_user)
 
-    user = len(json.loads(response_user.text))
+    user = json.loads(response_user.text)
     todo = json.loads(response_todo.text)
 
     todo_all = {}
-    for user_id in range(1, user + 1):
-        tasks = [i for i in todo if i['userId'] == user_id]
+    for user_id in range(1, len(user) + 1):
+        tasks = [i for i in todo if i.get('userId') == user_id]
+        for i in tasks:
+            i['task'] = i.pop('title')
+            i['username'] = user[user_id - 1].get('username')
+            i.pop('id')
+            i.pop('userId')
         todo_all[str(user_id)] = tasks
 
     with open('todo_all_employees.json', 'w') as f:
