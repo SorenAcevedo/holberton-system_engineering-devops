893d9410bf139dc98165244ce4f9e4d717e9839b
SorenAcevedo <sorenacevedo@gmail.com>
2021-05-03 11:52:14 -0500

    Add API project

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+import json
+
+
+if __name__ == "__main__":
+    url_name = 'https://jsonplaceholder.typicode.com/users/' + argv[1]
+    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
+
+    response_name = requests.get(url_name)
+    response_todo = requests.get(url_todo)
+
+    name = json.loads(response_name.text)['name']
+    todo = json.loads(response_todo.text)
+
+    tasks = [i for i in todo if i['userId'] == int(argv[1])]
+    complete_task = len([i for i in tasks if i['completed'] == True])
+    total_task = len(tasks)
+
+    final_response = 'Employee {} is done with tasks({}/{}):'.format(
+        name, complete_task, total_task)
+
+    print(final_response)
+    [print('    ' + i['title']) for i in tasks]
