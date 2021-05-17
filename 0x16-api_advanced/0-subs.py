b7ca4e0301ee1e5dfcc1d8e319b1a5e893ffaa49
SorenAcevedo <sorenacevedo@gmail.com>
2021-05-17 14:51:59 -0500

    Add 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,9 +9,7 @@ def number_of_subscribers(subreddit):
         0 if an invalid subreddit is given
         """
     url = "https://www.reddit.com/r/" + subreddit + "/about.json"
-    headers = {
-        'User-Agent': 'Chrome/90.0.4430.212 Safari/537.36'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, allow_redirects=False)
     if response.status_code != 200:
         return 0
     subs = response.json().get('data').get('subscribers')
