bcfb436d263e033af98095e4343a081ac343f6ff
SorenAcevedo <sorenacevedo@gmail.com>
2021-05-17 14:59:49 -0500

    Add 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,7 +12,9 @@ def top_ten(subreddit):
         Return that prints the title of the first 10 hot posts.
     """
     url = 'https://www.reddit.com/r/' + subreddit + '/hot.json?limit=10'
-    req = requests.get(url, allow_redirects=False)
+    headers = {
+        'User-Agent': 'Chrome/90.0.4430.212 Safari/537.36'}
+    req = requests.get(url, headers=headers, allow_redirects=False)
     if req.status_code != 200:
         print(None)
     else:
