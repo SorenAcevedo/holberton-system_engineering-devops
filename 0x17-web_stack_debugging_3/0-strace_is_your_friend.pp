a1983e1c41815eb073b6807b4d8a55dc02f02244
SorenAcevedo <sorenacevedo@gmail.com>
2021-05-19 16:05:39 -0500

    Add 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Change phpp by php, fix 500 error
-exec { 'fix bug'
+exec { 'fixbug'
   command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   provider => '/usr/bin:/usr/sbin:/bin',
 }
