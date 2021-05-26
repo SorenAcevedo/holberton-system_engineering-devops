359a54ef5f40e78d90d1066c2758668dff9f5f72
SorenAcevedo <sorenacevedo@gmail.com>
2021-05-26 10:42:30 -0500

    Add project

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# Change de ULIMIT and restart NGINX
+exec { 'sed -i s/15/5000/g /etc/default/nginx':
+  path     => '/usr/bin:/usr/sbin:/bin',
+  provider => shell,
+}
+
+exec { 'sudo service nginx restart':
+  path     => '/usr/bin:/usr/sbin:/bin',
+  provider => shell,
+}
